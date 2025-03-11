6a6b69b4851dee7dec6c1b3be0ef3ee3b9990f41
brainfreeze-ptr <samuel.smoleniak@hudekivestments.com>
2025-03-11 13:41:50 +0100

    add API methods for waiting list entries

--- src/components/ss-ambulance-wl-app/ss-ambulance-wl-app.tsx
+++ src/components/ss-ambulance-wl-app/ss-ambulance-wl-app.tsx
@@ -55,6 +55,7 @@ export class SsAmbulanceWlApp {
       <Host>
         { element === "editor"
         ? <ss-ambulance-wl-editor entry-id={entryId}
+            ambulance-id={this.ambulanceId} api-base={this.apiBase}
             oneditor-closed={ () => navigate("./list")} >
           </ss-ambulance-wl-editor>
         : <ss-ambulance-wl-list  ambulance-id={this.ambulanceId} api-base={this.apiBase} 
