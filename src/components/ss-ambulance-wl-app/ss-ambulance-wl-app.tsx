9b41c8874f50a2c97e23bd6f700ce12b02b88bb6
brainfreeze-ptr <samuel.smoleniak@hudekivestments.com>
2025-03-18 13:23:57 +0100

    Predpokladany cas vstupu

--- src/components/ss-ambulance-wl-app/ss-ambulance-wl-app.tsx
+++ src/components/ss-ambulance-wl-app/ss-ambulance-wl-app.tsx
@@ -37,6 +37,7 @@ export class SsAmbulanceWlApp {
   }
   
   render() {
+    console.debug("ss-ambulance-wl-app.render() - path: %s", this.relativePath);
     let element = "list"
     let entryId = "@new"
   
